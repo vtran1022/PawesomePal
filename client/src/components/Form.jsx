c8ede04b04c63b3d1352fba458fc3d57df4e0c78
Vy Tran <vtran1022@Gmail.com>
2021-07-08 12:11:08 -0700

    Updated format

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -8,24 +8,25 @@ const Form = () => {
         <InputLabel htmlFor="human-name">Name:</InputLabel>
         <Input id="human-name"></Input>
         <FormHelperText id="human-name-helper-text">Required</FormHelperText>
-
-        <TextField id="trait-select" label="Trait" value="20" select>
-          <MenuItem value="10">Ten</MenuItem>
-          <MenuItem value="20">Twenty</MenuItem>
-        </TextField>
-
-        <TextField id="species-select" label="Species" value="both" select>
-          <MenuItem id="cat" value="cat">Cat</MenuItem>
-          <MenuItem id="dog" value="dog">Dog</MenuItem>
-          <MenuItem id="both" value="both">Both</MenuItem>
-        </TextField>
-
-        <TextField id="gender-select" label="Gender" value="both" select>
-          <MenuItem id="male" value="M">M</MenuItem>
-          <MenuItem id="female" value="F">F</MenuItem>
-          <MenuItem id="both" value="both">Both</MenuItem>
-        </TextField>
       </FormControl>
+
+      <TextField id="trait-select" label="Trait" value="20" select>
+        <MenuItem value="10">Ten</MenuItem>
+        <MenuItem value="20">Twenty</MenuItem>
+      </TextField>
+
+      <TextField id="species-select" label="Species" value="both" select>
+        <MenuItem id="cat" value="cat">Cat</MenuItem>
+        <MenuItem id="dog" value="dog">Dog</MenuItem>
+        <MenuItem id="both" value="both">Both</MenuItem>
+      </TextField>
+
+      <TextField id="gender-select" label="Gender" value="both" select>
+        <MenuItem id="male" value="M">M</MenuItem>
+        <MenuItem id="female" value="F">F</MenuItem>
+        <MenuItem id="both" value="both">Both</MenuItem>
+      </TextField>
+
       <Button variant="contained" color="primary">
         Submit
       </Button>
