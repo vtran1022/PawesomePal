9493316c91160d8d41902f15050fa28c2cc61710
Vy Tran <vtran1022@Gmail.com>
2021-07-08 10:02:11 -0700

    added form file, added a butto

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
+
+const Form = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Submit
+    </Button>
+  )
+};
+
+export default Form;
