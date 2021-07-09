d865bfde8822e15c9be05651471a9d1d0ae790b8
Vy Tran <vtran1022@Gmail.com>
2021-07-08 17:54:40 -0700

    Added file for petMatch component

---
+++ client/src/components/PetMatch.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { Typography } from '@material-ui/core';
+
+const PetMatch = ({ pet }) => {
+  return (
+    <>
+      <span>Hello</span>
+    </>
+  )
+};
+
+export default PetMatch;
+
+/*
+"Congratulations <name>! You matched with <animal name>"
+
+primary_photo_cropped.large
+Name:
+Breed:
+Age:
+Gender:
+Size:
+*/
