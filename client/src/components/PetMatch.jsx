906baa612cbb4f7e52ead6ac6f58004f89e315ca
Vy Tran <vtran1022@Gmail.com>
2021-07-09 10:36:13 -0700

    styling

--- client/src/components/PetMatch.jsx
+++ client/src/components/PetMatch.jsx
@@ -2,63 +2,92 @@ import React, { useEffect, useState } from 'react';
 import errimage from './imagenot.png';
 import { Typography, Paper, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
-const pet = {
-  name: "Happy",
-  primary_photo_cropped: { large: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/52275445/1/?bust=1625708137&width=600"},
-  breeds: { primary: "Chihuahua" },
-  age: "Young",
-  gender: "Male",
-  size: "Medium",
-  description: "Lucky happy going happy papper",
-  url: "https://www.petfinder.com/dog/happy-52275445/ny/wappingers-falls/compassionate-animal-rescue-of-dutchess-county-ny1375/?referrer_id=4491b933-8254-4a5d-856b-8c34ef9bb017"
-};
-
-const name = "Fred";
+import Icon from '@material-ui/core/Icon';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    width: 600,
-    padding: theme.spacing(3)
+    width: 550,
+    padding: theme.spacing(6),
+    backgroundColor: theme.palette.action.hover
+  },
+  img: {
+    width: 450,
+    height: 450,
+    borderRadius: 30
   }
 }));
 
-const PetMatch = ({  }) => {
-  const [isLoaded, setLoaded] = useState(true);
+const PetMatch = ({ name, pet }) => {
+  const [isLoaded, setLoaded] = useState(false);
+  const [isTimed, setTime] = useState(false);
   const classes = useStyles();
 
-  // useEffect(() => {
-  //   Object.keys(pet).length !== 0
-  //     ? setLoaded(true)
-  //     : null;
-  // }, [pet]);
+  useEffect(() => {
+    Object.keys(pet).length !== 0
+      ? setLoaded(true)
+      : null;
+  }, [pet]);
+
+  useEffect(() => {
+    console.log(isLoaded);
+
+    isLoaded === true
+      ? setTimeout(() => {
+          setTime(true);
+        }, 2000)
+      : null;
+  }, [isLoaded]);
 
   return (
     <>
     {isLoaded
-      ? <>
-        <Grid container justifyContent="center">
+      ? <Grid container justifyContent="center">
           <Paper className={classes.paper}>
-            <Typography variant="body1">
-              Congratulations {name}! &nbsp; <i>-drumroll-</i> &nbsp; Your pawesome pal is {pet.name}!
-            </Typography>
+            {isTimed
+              ? <Grid container direction="column" alignItems="center">
+                  <Typography variant="body1">
+                    <Grid item>Congratulations {name}! &nbsp; <i>-drumroll-</i></Grid>
+                    <br></br>
+                    <Grid item>Your pawesome pal is <b>{pet.name}</b>!</Grid>
+                  </Typography>
 
-            <img src={pet.primary_photo_cropped ? pet.primary_photo_cropped.large : errimage}></img>
+                  <img className={classes.img} src={pet.primary_photo_cropped ? pet.primary_photo_cropped.large : errimage}></img>
 
-            <Typography variant="body1">{pet.description}</Typography>
-
-            <Typography variant="body2">
-              Breed: {pet.breeds.primary}
-              Age: {pet.age}
-              Gender: {pet.gender}
-              Size: {pet.size}
-              <br></br>
-              <br></br>
-              For more information, please <a href={pet.url}>click here.</a> You will be redirected to petFinder.
-            </Typography>
+                  <Typography variant="body1">{pet.description}</Typography>
+                  <br></br>
+                  <Typography variant="body2">
+                    <b>Breed:</b> {pet.breeds.primary}
+                    <br></br>
+                    <b>Age:</b> {pet.age}
+                    <br></br>
+                    <b>Gender:</b> {pet.gender}
+                    <br></br>
+                    <b>Size:</b> {pet.size}
+                    <br></br>
+                    <br></br>
+                    For more information, please <a href={pet.url}>click here.</a> You will be redirected to petFinder.
+                  </Typography>
+                </Grid>
+              : <Grid container justifyContent="center">
+                  <Typography variant="body1">
+                    Congratulations {name}! &nbsp; <i>-drumroll-</i>
+                    <br></br>
+                    <Grid item>
+                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                      <Icon className="fas fa-paw"></Icon>
+                    </Grid>
+                  </Typography>
+                </Grid>
+            }
           </Paper>
         </Grid>
-        </>
       : null
     }
     </>
