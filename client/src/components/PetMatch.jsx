c33e69a3a96b00a8306049ae058198222e65c5f8
Vy Tran <vtran1022@Gmail.com>
2021-07-09 11:03:21 -0700

    styling

--- client/src/components/PetMatch.jsx
+++ client/src/components/PetMatch.jsx
@@ -11,32 +11,44 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.action.hover
   },
   img: {
-    width: 450,
-    height: 450,
+    width: 470,
+    height: 470,
     borderRadius: 30
   }
 }));
 
-const PetMatch = ({ name, pet }) => {
-  const [isLoaded, setLoaded] = useState(false);
-  const [isTimed, setTime] = useState(false);
+  const name = "Fred";
+  const pet = {
+    name: "Bella",
+    description: "Available for Adoption \n“Bella”\n\n2 year 8 lb scruffy mix female. Chihuahua Yorkie  mix .She is a little reserved but...",
+    url: "https://www.petfinder.com/dog/bella-52288656/ky/winchester/fairytails-pet-adoptions-ky550/?referrer_id=4491b933-8254-4a5d-856b-8c34ef9bb017",
+    breeds: { primary: "Yorkshire Terrier" },
+    age: "young",
+    gender: "female",
+    size: "small",
+    primary_photo_cropped: { large: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/52288656/3/?bust=1625793457&width=600" }
+  };
+
+const PetMatch = ({  }) => {
+  const [isLoaded, setLoaded] = useState(true);
+  const [isTimed, setTime] = useState(true);
   const classes = useStyles();
 
-  useEffect(() => {
-    Object.keys(pet).length !== 0
-      ? setLoaded(true)
-      : null;
-  }, [pet]);
+  // useEffect(() => {
+  //   Object.keys(pet).length !== 0
+  //     ? setLoaded(true)
+  //     : null;
+  // }, [pet]);
 
-  useEffect(() => {
-    console.log(isLoaded);
+  // useEffect(() => {
+  //   console.log(isLoaded);
 
-    isLoaded === true
-      ? setTimeout(() => {
-          setTime(true);
-        }, 2000)
-      : null;
-  }, [isLoaded]);
+  //   isLoaded === true
+  //     ? setTimeout(() => {
+  //         setTime(true);
+  //       }, 2000)
+  //     : null;
+  // }, [isLoaded]);
 
   return (
     <>
@@ -46,14 +58,22 @@ const PetMatch = ({ name, pet }) => {
             {isTimed
               ? <Grid container direction="column" alignItems="center">
                   <Typography variant="body1">
-                    <Grid item>Congratulations {name}! &nbsp; <i>-drumroll-</i></Grid>
+                    <Grid item container justifyContent="center">
+                      Congratulations {name}!
+                    </Grid>
                     <br></br>
-                    <Grid item>Your pawesome pal is <b>{pet.name}</b>!</Grid>
+                    <Grid item>
+                      Your pawesome pal is <b>{pet.name}</b>!
+                    </Grid>
                   </Typography>
 
                   <img className={classes.img} src={pet.primary_photo_cropped ? pet.primary_photo_cropped.large : errimage}></img>
 
-                  <Typography variant="body1">{pet.description}</Typography>
+                  <Typography variant="body1">
+                    <Grid item style={{ 'width': 450 }}>
+                      {pet.description}
+                    </Grid>
+                  </Typography>
                   <br></br>
                   <Typography variant="body2">
                     <b>Breed:</b> {pet.breeds.primary}
@@ -68,17 +88,17 @@ const PetMatch = ({ name, pet }) => {
                     For more information, please <a href={pet.url}>click here.</a> You will be redirected to petFinder.
                   </Typography>
                 </Grid>
-              : <Grid container justifyContent="center">
+              : <Grid container justifyContent="center" alignItems="center">
                   <Typography variant="body1">
-                    Congratulations {name}! &nbsp; <i>-drumroll-</i>
+                    <Grid item container justifyContent="center">
+                      Congratulations {name}!
+                    </Grid>
+                      <br></br>
+                    <Grid item container justifyContent="center">
+                      <i>-drumroll-</i>
+                    </Grid>
                     <br></br>
-                    <Grid item>
-                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
-                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
-                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
-                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
-                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
-                      <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
+                    <Grid item container justifyContent="center">
                       <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
                       <Icon className="fas fa-paw" style={{ 'marginRight': 5 }}></Icon>
                       <Icon className="fas fa-paw"></Icon>
