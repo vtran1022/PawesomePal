849570f6c93052b7061460206c4307ba348dac8c
Vy Tran <vtran1022@Gmail.com>
2021-08-11 21:02:07 -0700

    Adjusted code to do error handling and remove promise anti-pattern

--- server/controller.js
+++ server/controller.js
@@ -5,24 +5,31 @@ const getTraits = (req, res) => {
   fetchPersonalityTraits()
     .then((data) => res.status(200).send(data))
     .catch((err) => {
-      res.status(500).send(`Error fetching traits: ${err}`)
+      res.status(500).send(`Error fetching traits: ${err}`);
       console.error(`Error fetching traits: ${err}\n\n${err.stack}`);
     });
 };
 
 const getPetMatch = (req, res) => {
+  const trait = req.query.trait_id;
+  const type = req.query.type;
   let gender = req.query.gender;
+
+  if (!trait || !type || !gender) {
+    res.status(400).send(`Request missing information: ${err}`);
+  }
+
   if (gender === 'both') {
     gender = 'male,female';
   }
 
-  fetchBreedInfo(req.query.trait_id, req.query.type)
+  fetchBreedInfo(trait, type)
     .then((breeds) => {
-      fetchPet(breeds, gender)
-      .then((data) => res.status(200).send(data))
+      return fetchPet(breeds, gender);
     })
+    .then((pet) => res.status(200).send(pet))
     .catch((err) => {
-      res.status(500).send(`Error fetching pets: ${err}`)
+      res.status(500).send(`Error fetching pets: ${err}`);
       console.error(`Error fetching pets: ${err}\n\n${err.stack}`);
     });
 };
